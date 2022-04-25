2663a5d0ed2b254339d162fcd4bc5d2e78a4a4b6
tripper123 <tripper123@o2.pl>
2022-04-25 22:54:11 +0200

    dodanie danych z cenami, ranking cen

--- loty/views.py
+++ loty/views.py
@@ -154,6 +154,7 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
+
                     return HttpResponse('ZALOGOWANY')
                 else:
                     return HttpResponse('BŁĄD LOGOWANIA')
