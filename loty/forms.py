7b6d3101718ebf263729a41df7a26d0e0fd71acd
tripper123 <tripper123@o2.pl>
2022-04-27 23:19:34 +0200

    poprawa wyglądu strony

---
+++ loty/forms.py
@@ -0,0 +1,27 @@
+from .models import Loty, Kraje, Linie
+from django import forms
+from django.forms import DateInput
+
+class LotyForm(forms.ModelForm):
+    lotnisko_wylot= forms.CharField(max_length=32)
+    lotnisko_przylot = forms.CharField(max_length=32)
+    data_lotu = forms.DateField(widget=DateInput(
+                format=('%d/%m/%Y'),
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Select a date',
+                    'type': 'date',}
+    ))
+
+    def __init__(self, *args, **kwargs):
+        super(LotyForm,self).__init__(*args, **kwargs)
+        self.fields['linia'] = forms.ModelChoiceField(queryset=Linie.objects.all())
+        self.fields['kraj'] = forms.ModelChoiceField(queryset=Kraje.objects.all())
+
+    class Meta:
+        model = Loty
+        fields = ('lotnisko_wylot', 'lotnisko_przylot', 'data_lotu', 'kraj' , 'linia' )
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
---
+++ loty/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class Linie(models.Model):
+    STATUS_CHOICES =( ('', ''), ('niskokosztowa','Niskokosztowa'), ('tradycyjna', 'Tradycyjna'), ('czarterowa', 'Czarterowa'))
+    nazwa_linii = models.CharField(max_length=32)
+    rodzaj_linii = models.CharField(max_length=32, choices=STATUS_CHOICES, default='')
+    def __str__(self):
+        return f'{self.nazwa_linii} '
+    class Meta:
+        verbose_name="Linia"
+        verbose_name_plural="Linie"
+
+class Kraje(models.Model):
+    STATUS_CHOICES = (
+    ('', ''), ('europa', 'Europa'), ('azja', 'Azja'),('afryka', 'Afryka'), ('ameryka południowa', 'Ameryka Południowa'),('australia', 'Australia'), \
+    ('ameryka północna', 'Ameryka Północna'))
+    nazwa_kraju = models.CharField(max_length=32)
+    kontynent = models.CharField(max_length=32, choices=STATUS_CHOICES, default='')
+
+    def __str__(self):
+        return f'{self.nazwa_kraju}'
+
+    class Meta:
+        verbose_name="Kraj"
+        verbose_name_plural="Kraje"
+
+
+class Loty(models.Model):
+    lotnisko_wylot = models.CharField(max_length=32)
+    lotnisko_przylot = models.CharField(max_length=32)
+    data_lotu = models.DateField()
+    kraj = models.ForeignKey(Kraje, on_delete=models.PROTECT, blank=True, null=True)
+    linia = models.ForeignKey(Linie, on_delete=models.PROTECT, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.lotnisko_wylot} {self.lotnisko_przylot} {self.data_lotu} {self.kraj} {self.linia}'
+
+    class Meta:
+        verbose_name="Lot"
+        verbose_name_plural="Loty"
